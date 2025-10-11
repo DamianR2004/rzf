77e783630b47ce9d2744569d1de2f63496972acf
Damian <144007686+DamianR2004@users.noreply.github.com>
2025-10-11 15:44:31 +0200

    Update v0.4

---
+++ myapp/src/lib/molecules/Header.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="#home"></a></li>
+        <li><a href="/portfolio"></a></li>
+        <li><a href="#over-mij"></a></li>
+        <li><a href="#contact"></a></li>
+    </ul>
+</nav>
